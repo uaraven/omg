c068a856af4d589551d25908a147751c2dcfaa04
Oleksii Voronin <ovoronin@gmail.com>
2019-10-18 21:37:59 -0400

    Add license information
    It is time to add license.

--- src/test/java/net/ninjacat/omg/bytecode/ObjectPatternTest.java
+++ src/test/java/net/ninjacat/omg/bytecode/ObjectPatternTest.java
@@ -28,35 +28,6 @@ public class ObjectPatternTest {
 
         assertThat(match, is(true));
     }
-//
-//    @Test
-//    public void testLogicalMatching() {
-//        final Condition condition = Conditions.matcher()
-//                .property("inner").match(obj ->
-//                        obj.or(orCond -> orCond
-//                                .property("aString").eq("found it")
-//                                .property("aString").regex(".*works.*"))
-//                )
-//                .property("name").neq("Waldo")
-//                .build();
-//
-//        final Pattern<TestClass> pattern = Patterns.compile(condition, AsmPatternCompiler.forClass(TestClass.class));
-//
-//        final List<TestClass> testList =
-//                io.vavr.collection.List.of(
-//                        new TestClass(new InnerClass(1, "found it"), "Waldo"),
-//                        new TestClass(new InnerClass(1, "works!"), "Waldo"),
-//                        new TestClass(new InnerClass(1, "found it"), "Joseph"),
-//                        new TestClass(new InnerClass(1, "it works!"), "Yoda")
-//                ).asJava();
-//
-//        final List<TestClass> matched = testList.stream().filter(pattern::matches).collect(Collectors.toList());
-//
-//        assertThat(matched, hasItems(
-//                new TestClass(new InnerClass(1, "found it"), "Joseph"),
-//                new TestClass(new InnerClass(1, "it works!"), "Yoda")
-//        ));
-//    }
 
     @Value.Immutable
     @Immutable
